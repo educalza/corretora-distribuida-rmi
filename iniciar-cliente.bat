fe5792985da1ca8b67468042859278dfaf14eb37
vieiranaju <anajuvieira2005@gmail.com>
2026-04-05 23:37:17 -0300

    caras

--- iniciar-cliente.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Iniciando Cliente da Corretora RMI...
-cd /d "%~dp0"
-java -cp out Cliente
-pause
--- iniciar-servidor.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Iniciando Servidor da Corretora RMI...
-cd /d "%~dp0"
-java -cp out Servidor
-pause
